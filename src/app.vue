c8113612ded6a42c5d6bfd7cb65c57d1275b67c9
fengxudong1 <fengxudong@jd.com>
2017-09-20 16:41:13 +0800

    新建模板

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
+    <div class="root">
         <router-view></router-view>
     </div>
 </template>
+<style lang="less">
+    body,html{
+        height: 100%;
+    }
+		.root{
+			height: 100%;
+		}
+</style>
+
 <script>
     export default {
         data() {
--- src/router.js
+++ src/router.js
@@ -4,5 +4,11 @@ const routers = [{
         title: ''
     },
     component: (resolve) => require(['./views/index.vue'], resolve)
+},{
+    path: '/template',
+    meta: {
+        title: 'template'
+    },
+    component: (resolve) => require(['./views/template.vue'], resolve)
 }];
 export default routers;
