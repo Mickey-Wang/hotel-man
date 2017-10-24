c6caff79a87002a91055e2396111a94f87bd95a6
zhangyanyan <zhangyanyan3@jd.com>
2017-10-24 15:28:21 +0800

    :sparkles: 城市页面修复

--- src/views/componets/tables/cityExamine.vue
+++ src/views/componets/tables/cityExamine.vue
@@ -3,12 +3,12 @@
         <div class="topTable">
             <div class="title">城市审核列表</div>
             <div class="button">
-                <Button type="primary" @click="toSubmit1" v-if="cityApprovalList">提交</Button>
-                <Button type="primary" disabled v-if="!cityApprovalList">提交</Button>
-                <Button type="primary" @click="toSubmit2" v-if="cityTableType!=10 && cityApprovalList">设为待审</Button>
-                <Button type="primary" disabled v-if="!cityApprovalList">设为待审</Button>
-                <Button type="primary" v-if="cityApprovalList">新增</Button>
-                <Button type="primary" disabled v-if="!cityApprovalList">新增</Button>
+                <Button type="primary" @click="toSubmit1" v-if="cityApprovalList.length!=0">提交</Button>
+                <Button type="primary" disabled v-if="cityApprovalList.length==0">提交</Button>
+                <Button type="primary" @click="toSubmit2" v-if="cityTableType!=10 && cityApprovalList.length!=0">设为待审</Button>
+                <Button type="primary" disabled v-if="cityApprovalList.length==0">设为待审</Button>
+                <Button type="primary" v-if="cityApprovalList.length!=0">新增</Button>
+                <Button type="primary" disabled v-if="cityApprovalList.length==0">新增</Button>
             </div>
             <div class="total">共计XX条</div>
             <div class="table table1">
