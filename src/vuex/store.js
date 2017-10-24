2fb74e65b4aad560f323ba39f2c16a75688dfae2
fengxudong1 <fengxudong@jd.com>
2017-10-24 21:13:00 +0800

    :spark:改为新接口

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -8,20 +8,20 @@ Vue.use(Vuex);
 
 let state = {
   //异步加载loading
-  asyncLoading : false,
+    asyncLoading : false,
   //主导航状态同步
-  routerName:'',
+    routerName:'',
   //10:未聚待审;20:已聚待审;30:已聚已审(默认未聚待审)
-  cityTableType:20,
-  hotelTableType:20,
-  roomTableType:20,
-  cityCheckList:{},
-  hotelCheckList:{},
-  roomCheckList:{}
+    cityTableType:20,
+    hotelTableType:20,
+    roomTableType:20,
+    cityCheckList:{},
+    hotelCheckList:{},
+    roomCheckList:{}
 };
 export default new Vuex.Store({
-  state,
-  actions,
-  getters,
-  mutations
+    state,
+    actions,
+    getters,
+    mutations
 });
