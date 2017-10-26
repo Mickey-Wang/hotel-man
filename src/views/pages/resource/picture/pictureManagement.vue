086f86b6fa5fa27d3ff0c243b36b10f76a5a4a40
fengxudong1 <fengxudong@jd.com>
2017-10-26 14:50:49 +0800

    :sparkles:新增图片和目的地路由和页面

---
+++ src/views/pages/resource/picture/pictureManagement.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="layout-row">
+        <Row class="layout-row">
+            图片
+        </Row>
+    </div>
+
+</template>
+<style lang="less" scoped>
+    .layout-row {
+        height: 100%;
+    }
+    @height: 100%;
+    .layout-content-main {
+        padding: 10px;
+        height: @height;
+        .layout-content-main-base {
+            height: @height;
+            border: 1px dashed #ddd;
+            padding: 2px;
+        }
+        .layout-content-main-left {
+            .layout-content-main-base;
+        }
+        .layout-content-main-right {
+            .layout-content-main-base;
+        }
+    }
+</style>
+
+<script>
+    
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        components: {
+            
+        }
+    }
+</script>
